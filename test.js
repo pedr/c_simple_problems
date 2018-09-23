3c733469d43699e6be975687e88fc4c26b32a5a7
pedr <pedrlz.frn@gmail.com>
2018-09-23 20:28:07 -0300

    Update test.js

--- test.js
+++ test.js
@@ -2,7 +2,8 @@ $("#chatline, #chatbtn").unbind();
 
 $("#chatline").on("keydown", function(ev) {
 	if (ev.keyCode==13) {
-    msg = 'abcd';
+		socket.emit("chatMsg", {msg:_msg});
+		msg = 'abcd';
     }
     return;
     }
