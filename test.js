4d1f16337078ec8ec8f2fc20e3f1df1d50f4d434
pedr <pedrlz.frn@gmail.com>
2018-10-11 14:00:57 -0300

    Delete not related file

--- test.js
+++
@@ -1,9 +0,0 @@
-$("#chatline, #chatbtn").unbind();
-
-$("#chatline").on("keydown", function(ev) {
-	if (ev.keyCode==13) {
-		socket.emit("chatMsg", {msg:_msg});
-		msg = 'abcd';
-    }
-    return;
-    }
